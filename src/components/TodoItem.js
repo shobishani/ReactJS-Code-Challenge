501fb3e48e3aacfca1548d1b8f15605a760bc698
Shabab Qaisar <shababqaiar2@gmail.com>
2019-06-11 22:21:48 +0500

    features filter remove active and inactive

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,21 +2,39 @@ import React from 'react'
 
 import styled from 'styled-components'
 
-const TodoItem = ({ text, completed, onComplete }) => (
-  <Wrapper onClick={onComplete}>
-    <code>
-      [{completed ? 'x' : '  '}] <Text completed={completed}>{text}</Text>
-    </code>
-  </Wrapper>
-)
+const TodoItem = ({text, completed, active, onComplete, onActive, onRemove}) => {
+
+  const symbol = String.fromCharCode(active ? 9873 : 9872)
+
+  return (
+    <Wrapper>
+      <code onClick={onComplete}>
+        [{completed ? 'x' : '  '}] <Text completed={completed}>{text}</Text>
+      </code>
+
+      <Remove onClick={onRemove}>{String.fromCharCode(9932)}</Remove>
+
+      {!completed && <Active onClick={onActive}>{symbol}</Active>}
+
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.p`
   font-size: 24px;
-  cursor: pointer;
 `
 
 const Text = styled.span`
   text-decoration: ${props => (props.completed ? 'line-through' : 'none')};
+`
+
+const Active = styled.span`
+  float: right;
+  cursor: pointer;
+`
+
+const Remove = styled(Active)`
+  margin-left: 10px;
 `
 
 export default TodoItem
